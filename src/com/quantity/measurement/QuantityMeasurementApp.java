ac612c2943d32a40826debf46aa7d42a98734056
Your Name <aniviyaksivarajan2005@gmail.com>
2026-04-27 15:40:25 +0530

    UC8: Refactored LengthUnit to standalone enum with conversion responsibility

--- src/com/quantity/measurement/QuantityMeasurementApp.java
+++ src/com/quantity/measurement/QuantityMeasurementApp.java
@@ -2,18 +2,21 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=== UC7: Addition with Target Unit ===");
+        System.out.println("=== UC8 Refactored Design ===");
 
         QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
         QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCHES);
 
-        // UC7: Explicit target unit
-        QuantityLength result1 = QuantityLength.add(q1, q2, LengthUnit.FEET);
-        QuantityLength result2 = QuantityLength.add(q1, q2, LengthUnit.INCHES);
-        QuantityLength result3 = QuantityLength.add(q1, q2, LengthUnit.YARDS);
+        // Conversion
+        System.out.println(q1.convertTo(LengthUnit.INCHES));
 
-        System.out.println("Result in FEET: " + result1);
-        System.out.println("Result in INCHES: " + result2);
-        System.out.println("Result in YARDS: " + result3);
+        // Equality
+        System.out.println("Equal: " + q1.equals(q2));
+
+        // Addition
+        QuantityLength result =
+                QuantityLength.add(q1, q2, LengthUnit.FEET);
+
+        System.out.println("Addition: " + result);
     }
 }

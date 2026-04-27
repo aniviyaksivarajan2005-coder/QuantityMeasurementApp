28234ef83629e8660819e80e221eb7a16de4ee42
Your Name <aniviyaksivarajan2005@gmail.com>
2026-04-27 15:10:25 +0530

    UC5: Implement static convert API with validation and precision handling

--- src/com/quantity/measurement/LenghtUnit.java
+++ src/com/quantity/measurement/LenghtUnit.java
@@ -3,9 +3,9 @@ package com.quantity.measurement;
 public enum LengthUnit {
 
     FEET(1.0),
-    INCH(1.0 / 12),
-    YARD(3.0),
-    CM(1.0 / 30.48);
+    INCHES(1.0 / 12),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 30.48);
 
     private final double toFeetFactor;
 
